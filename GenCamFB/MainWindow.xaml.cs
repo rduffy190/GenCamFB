704190eee36a2a3fe02a3d0247b506d6aac2e453
Duffy Riley (DCNA/SLR23-US) <Riley.Duffy@boschrexroth-us.com>
2024-05-16 09:05:27 -0400

    Test GIt

--- GenCamFB/MainWindow.xaml.cs
+++ GenCamFB/MainWindow.xaml.cs
@@ -19,9 +19,9 @@ namespace GenCamFB
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CamToFB : Window
     {
-        public MainWindow()
+        public CamToFB()
         {
             InitializeComponent();
 
